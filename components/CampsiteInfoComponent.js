a756c6e6180559e29dc30741a600151171ad2e06
Jemal Hassen <jemalhassen@Jemals-MBP.lan>
2020-09-14 22:03:55 -0700

    week 1 asignment and week 2 first videio

---
+++ components/CampsiteInfoComponent.js
@@ -0,0 +1,76 @@
+import React, {Component} from 'react';
+import { Text, View, ScrollView, FlatList, TouchableNativeFeedbackComponent} from 'react-native';
+import { Card, Icon } from 'react-native-elements';
+import { CAMPSITES} from '../Shared/campsites';
+import { COMMENTS } from '..git/shared/comments';
+
+
+
+function RenderComments({comments}) {
+
+    const renderCommentItem = ({comments}) => {
+        return (
+            <View style= {{margin:10}}>
+              <Text style ={{fontSize:14}}>{item.text}</Text>
+              <Text style ={{fontSize:12}}>{item.rating} Stars</Text>
+              <Text style ={{fontSize:12}}>{`--${item.author}, ${item.date}`}</Text>
+            </View>
+        );
+    }
+      return (
+          <Card title='Comments'>
+            <FlatList
+               data = {comments}
+               renderItem = {renderCommentItem}
+               keyExtractor = {item => item.id.toString()}
+             
+             />
+
+            
+          </Card>
+      );
+}
+
+function RenderCampsite({campsite}) {
+    if (campsite) {
+        return (
+            <Card
+                featuredTitle={campsite.name}
+                image={require('./images/react-lake.jpg')}>
+                <Text style={{margin: 10}}>
+                    {campsite.description}
+                </Text>
+            </Card>
+        );
+    }
+    return <View />;
+}
+
+class CampsiteInfo extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = { 
+            campsites: CAMPSITES,
+            comments: COMMENTS
+        };
+    }
+  static navigationOption = {
+      title: 'Campsite Information'
+  }
+
+    render() {
+        const campsiteId = this.props.navigation.getParam('campsiteId');
+        const campsite = this.state.campsites.filter(campsite =>campsite.id ===campsiteId)[0];
+        const comments = this.state.comments.filter(comments=>comments.campsite.id === campsiteID);
+    return (
+        <ScrollView>
+              <RenderCampsite campsite= {campsite} />
+              <RenderComments comments = {comments} /> 
+         </ScrollView>
+      );
+    }
+    
+}
+
+export default CampsiteInfo;
---
+++ components/MainComponent.js
@@ -0,0 +1,71 @@
+import Home from "./HomeComponent";
+import Directory from "./DirectoryComponent";
+import CampsiteInfo from "./CampsiteInfoComponent";
+import About from "./AboutComponent";
+import Contact from "./ContactComponent";
+import { View, Platform } from "react-native";
+import { createStackNavigator, createDrawerNavigator } from "react-navigation";
+
+const DirectoryNavigator = createStackNavigator(
+    {
+        Directory: { screen: Directory },
+        CampsiteInfo: { screen: CampsiteInfo }
+    },
+    {
+        initialRouteName: 'Directory',
+        navigationOptions: {
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
+        }
+    }
+);
+
+const MainNavigator = createDrawerNavigator(
+    {
+        Home: { screen: HomeNavigator },
+        Directory: { screen: DirectoryNavigator }
+    },
+    {
+        drawerBackgroundColor: '#CEC8FF'
+    }
+  );
+
+const HomeNavigator = createStackNavigator(
+    {
+        Home: { screen: Home }
+    },
+    {
+        navigationOptions: {
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
+        }
+    }
+);
+
+
+
+class Main extends Component {
+    render() {
+        return (
+            <View style={{
+                flex: 1,
+                paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight
+            }}>
+                <MainNavigator />
+            </View>
+        );
+    }
+}
+
+export default Main;
+
